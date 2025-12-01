43cad773bcc96510eea167dd8666792ac865ab98
Jimmyopot <jimmyopot@gmail.com>
2025-12-01 14:19:15 +0300

    Update .gitignore to exclude config and documentation files

--- src/utils/config.js
+++
@@ -1,7 +0,0 @@
-// import { fromUnixTime } from "date-fns";
-
-export const config = {
-  apiUrl: "http://localhost:5043/api/",
-  VITE_BACKEND_URL: "http://localhost:5043",
-  VITE_SIGNALR_HUB_URL: "http://localhost:5043/hubs/chat"
-};
